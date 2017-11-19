a1d6ceea431b6bbe7d38d639c5cc04d4224398a3
Gillian Pierce <gmpierce@ualberta.ca>
2017-11-19 02:12:09 -0700

    database connection is FINALLY WORKING

--- app/src/main/java/com/collabify/collabify/EnterIDActivity.java
+++ app/src/main/java/com/collabify/collabify/EnterIDActivity.java
@@ -3,27 +3,59 @@ package com.collabify.collabify;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+
+import java.util.ArrayList;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class EnterIDActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.tralg.collabify.MESSAGE";
+    public static final String SONG_ARRAY = "com.tralg.collabify.SONGS";
+
+    Button joinRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_id);
+
+        joinRoom = (Button) findViewById(R.id.join);
+
+//    joinRoom.setOnClickListener(new View.OnClickListener(){
+//        @Override
+//        public void onClick(View view) {
+////            Intent intent = new Intent(this, QueueActivity.class);
+//            EditText roomID = (EditText) findViewById(R.id.enterID);
+//            String message = roomID.getText().toString();
+//            Room r = Room.getRoomFromID("-KzHKTTBKiazjmImP-KR", rooms);
+//            Log.d("help", r.toString());
+//            ArrayList<Song> songs = r.songs;
+//            for(Song s: songs){
+//                Log.d("help: ", s.toString());
+//            }
+////            intent.putExtra(EXTRA_MESSAGE, message);
+////            intent.putExtra(SONG_ARRAY, songs);
+////            startActivity(intent);
+//        }
+//    });
+
+
     }
 
     public void joinRoom(View view){
         Intent intent = new Intent(this, QueueActivity.class);
         EditText roomID = (EditText) findViewById(R.id.enterID);
         String message = roomID.getText().toString();
+
         intent.putExtra(EXTRA_MESSAGE, message);
+
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/collabify/collabify/QueueActivity.java
+++ app/src/main/java/com/collabify/collabify/QueueActivity.java
@@ -10,12 +10,14 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -28,27 +30,35 @@ public class QueueActivity extends AppCompatActivity {
     private List<String> mUris;
     private RecyclerView.LayoutManager mLayoutManager;
     private boolean isHost;
-    private boolean isPlaying = false;
+    private boolean isPlaying = true;
     private ImageButton playButton;
+    private ArrayList<? extends Song> SongList;
+    private Button refreshButton;
+    ArrayList<User> users=new ArrayList<>();
+    ArrayList<Room> rooms=new ArrayList<>();
+    ArrayList<Song> songs;
+    Database data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
-
+        data = new Database(this);
+        data.readData(users, rooms);
         Intent intent = getIntent();
         TextView roomID = findViewById(R.id.RoomID);
         isHost = intent.getBooleanExtra(HostAndJoinActivity.IS_HOST, false);
+        String ID;
         if(isHost){
-            String ID = intent.getStringExtra(HostAndJoinActivity.EXTRA_MESSAGE);
+            ID = intent.getStringExtra(HostAndJoinActivity.EXTRA_MESSAGE);
             roomID.setText(ID);
         } else{
-            String ID = intent.getStringExtra(EnterIDActivity.EXTRA_MESSAGE);
+            ID = intent.getStringExtra(EnterIDActivity.EXTRA_MESSAGE);
             roomID.setText(ID);
         }
         Button addSong = (Button)findViewById(R.id.addSong);
         playButton = (ImageButton)findViewById(R.id.playButton);
-
+        refreshButton = (Button)findViewById(R.id.refreshButton);
         mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -57,6 +67,20 @@ public class QueueActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Room r = Room.getRoomFromID("-KzHKTTBKiazjmImP-KR", rooms);
+                Log.d("help", r.toString());
+                songs = r.songs;
+                refreshButton.setVisibility(View.GONE);
+                mRecyclerView.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
         mItems = new ArrayList<>();
         //adding test items to the list
         mAdapter = new CustomAdapter(this, mItems);
@@ -74,12 +98,19 @@ public class QueueActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(isPlaying){
-                    playButton.setBackgroundResource(17301539);
+                    playButton.setImageResource(android.R.drawable.ic_media_pause);
+                } else {
+                    playButton.setImageResource(android.R.drawable.ic_media_play);
                 }
+                isPlaying = !isPlaying;
             }
         });
 
 
 
+    }
+
+    public void doClick(){
+        refreshButton.performClick();
     }
 }
